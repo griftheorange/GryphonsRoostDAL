89e2faf11ab636f7c198d9e1db67a66b53854ba1
griftheorange <griffinpoole5@gmail.com>
2020-12-18 21:32:43 -0500

    Initial Commit, Database connection established for localhost connection

---
+++ dal/src/main/java/com/gryphonsroost/dal/DalApplication.java
@@ -0,0 +1,14 @@
+package com.gryphonsroost.dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
